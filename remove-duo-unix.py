5b975f94866980167d7bdb6dd7f4679f516f4cc0
Skip Cruse <f8al.err0r@gmail.com>
2015-10-28 12:37:53 -0500

    adding duo unix

---
+++ remove-duo-unix.py
@@ -0,0 +1,27 @@
+#!/bin/python
+from fabric.api import *
+from fabric.contrib.files import exists, sed
+
+
+env.shell = "/bin/bash -c"
+env.user="ssh_user"
+env.password = "changeme"
+env.sudo_password="changeme"
+env.dedupe_hosts = "True"
+env.port= "22"
+env.sudo_user = "root"
+env.colorize_errors = "True"
+env.ignore_hostkey="True"
+
+@task()
+@parallel
+def remove_duo_unix():
+	"Removes duo unix integration"
+	if exists('/etc/duo',use_sudo=True,verbose=False):
+		with cd('/etc/'):
+			sudo('logger "Removing duo configs"')
+			sudo('rm -rfv duo/')
+	if exists('/usr/sbin/login_duo',use_sudo=True,verbose=False):
+		sudo('rm /usr/sbin/login_duo -rf')
+	sed('/etc/ssh/sshd_config','ForceCommand /usr/sbin/login_duo','\n',limit='',use_sudo=True,backup='bak' )
+	sudo('service sshd restart')
