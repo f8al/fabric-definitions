1889f8245d78eab159c5cd505dc260598ed8bf45
Skip Cruse <f8al.err0r@gmail.com>
2015-10-28 14:09:36 -0500

    updating duo unix

--- remove-duo-unix.py
+++ remove-duo-unix.py
@@ -13,15 +13,18 @@ env.sudo_user = "root"
 env.colorize_errors = "True"
 env.ignore_hostkey="True"
 
+
 @task()
 @parallel
 def remove_duo_unix():
 	"Removes duo unix integration"
+	sed('/etc/ssh/sshd_config','ForceCommand /usr/sbin/login_duo','     ',limit='',use_sudo=True,backup='bak' )
+	sudo('service sshd restart')
 	if exists('/etc/duo',use_sudo=True,verbose=False):
-		with cd('/etc/'):
+		with cd('/etc'):
 			sudo('logger "Removing duo configs"')
 			sudo('rm -rfv duo/')
 	if exists('/usr/sbin/login_duo',use_sudo=True,verbose=False):
-		sudo('rm /usr/sbin/login_duo -rf')
-	sed('/etc/ssh/sshd_config','ForceCommand /usr/sbin/login_duo','\n',limit='',use_sudo=True,backup='bak' )
-	sudo('service sshd restart')
+		with cd('/usr/sbin'):
+			sudo('logger "Removing login_duo"')
+			sudo('rm -frv login_duo')
