06d0a57a86d5c05a177217578e31093ed62c3105
Skip Cruse <f8al.err0r@gmail.com>
2016-09-16 21:28:58 -0500

    fabric definition to install saltstacks master and minion daemon

---
+++ install_configure_salt_master.py
@@ -0,0 +1,26 @@
+#!/bin/python
+from fabric.api import *
+from fabric.contrib.files import exists
+
+env.shell = "/bin/bash -c"
+env.user="ssh_user"
+env.password = "changeme"
+env.sudo_password="changeme"
+env.dedupe_hosts = "True"
+env.port= "22"
+env.sudo_user = "root"
+env.colorize_errors = "True"
+env.ignore_hostkey="True"
+
+@task(alias='configure-minion')
+@parallel
+def configure_salt_minion_master():
+	"modifies config to point to salt master"
+	sudo('yum install -y https://repo.saltstack.com/yum/redhat/salt-repo-latest-1.el7.noarch.rpm ')
+	sudo('yum clean expire-cache')
+	sudo('yum install salt-minion salt-master -y')
+	sed('/etc/salt/minion','#master: salt','master: <MASTER IP HERE>',limit='',use_sudo=True,backup='bak' )
+	sudo('chkconfig salt-minion on')
+	sudo('chkconfig salt-master on')
+	sudo('service salt-minion start')
+	sudo('service salt-master start')
